24c5a4000aa550a528225dc336642f034b6e241d
ipg <>
2023-07-05 13:50:46 +0300

    sem8 init

---
+++ zad62/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 62
+
+
+
+void ChangeMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+//Генерация массива
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    var rand = new Random();
+    for (int i = 0; i < tempMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < tempMatrix.GetLength(1); j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return tempMatrix;
+}
+//Ввод данных
+int[] ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Array.ConvertAll(Console.ReadLine()!.Split(","), int.Parse); ;
+}
+// Печать двумерного массива
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+//..........................
+int[] size = ReadInt("Задайте количество строк и столбцов через запятую: ");
+int[] range = ReadInt("Задайте левую и правую границы массива через запятую:  ");
+int[,] matrix = FillMatrix(size[0], size[1], range[0], range[1]);
+
+PrintMatrix(matrix);
+
+ChangeMatrix(matrix);
+System.Console.WriteLine("");
+PrintMatrix(matrix);
