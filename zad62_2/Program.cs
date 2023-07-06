345cc7947ef84b2385680431b71e881923ce0f43
ipg <>
2023-07-06 14:22:47 +0300

    доп вариант по задаче 62

---
+++ zad62_2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Это - вариант под произвольный массив
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int main()
+{
+    int a[10][10] = {0};
+    int N, M;
+
+    scanf("%d%d", &N, &M);
+    
+    int Ibeg = 0, Ifin = 0, Jbeg = 0, Jfin = 0;
+    
+    int k = 1;
+    int i = 0;
+    int j = 0;
+
+    while (k <= N * M){
+        a[i][j] = k;
+        if (i == Ibeg && j < M - Jfin - 1)
+            ++j;
+        else if (j == M - Jfin - 1 && i < N - Ifin - 1)
+            ++i;
+        else if (i == N - Ifin - 1 && j > Jbeg)
+            --j;
+        else
+            --i;
+
+        if ((i == Ibeg + 1) && (j == Jbeg) && (Jbeg != M - Jfin - 1)){
+            ++Ibeg;
+            ++Ifin;
+            ++Jbeg;
+            ++Jfin;
+        }
+        ++k;
+    }
+    
+    for (int i = 0; i < 10; ++i){
+        for (int j = 0; j < 10; ++j)
+            printf("%3d", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
